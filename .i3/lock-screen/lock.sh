cd5c24a5892602f0fe8488e16e6b69d6424eae55
Hugo Chinchilla Carbonell <hchinchilla@habitissimo.com>
2016-11-02 09:07:01 +0100

    add DPMS support to screen locker script

--- .i3/lock-screen/lock.sh
+++ .i3/lock-screen/lock.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+revert() {
+	xset dpms 0 0 0
+}
+trap revert SIGHUP SIGINT SIGTERM
+
+
 icon="$HOME/.i3/lock-screen/1.png"
 icon_alt="$HOME/.i3/lock-screen/2.png"
 
@@ -16,5 +22,13 @@ tmpbg='/tmp/screen.png'
 scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+
+# Pause Spotify
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause
-i3lock -i "$tmpbg" -t -u
+# Enable DPMS to stop displays on inactivity
+# xset +dpms dpms (standby time) (suspend time) (off time)
+xset +dpms dpms 0 60 60
+# Lock the screen
+i3lock -i "$tmpbg" -t -u -n
+# Deactivate DPMS
+revert
