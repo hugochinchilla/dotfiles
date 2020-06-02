546d914f62bb29e540383f55b208d3e45c85a91e
Hugo Chinchilla <hugoasecas@gmail.com>
2020-06-02 12:51:59 +0200

    separate DPMS config from the screen locker script

--- i3/.config/i3/scripts/lock-screen.sh
+++ i3/.config/i3/scripts/lock-screen.sh
@@ -14,11 +14,6 @@ if pgrep i3lock > /dev/null; then
     exit 0
 fi
 
-revert() {
-	xset dpms 0 0 0
-}
-trap revert SIGHUP SIGINT SIGTERM
-
 
 icon="$dir/../screenlocker/1.png"
 icon_alt="$dir/../screenlocker/2.png"
@@ -37,9 +32,6 @@ convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 
 # Pause Spotify
 dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause > /dev/null 2>&1 &
-# Enable DPMS to stop displays on inactivity
-# xset +dpms dpms (standby time) (suspend time) (off time)
-xset +dpms dpms 0 60 60
 
 
 i3lock -i "$tmpbg" -t -u $i3flags
