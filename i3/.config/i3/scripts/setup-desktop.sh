ec5f15829d44a1977f343cc5372010f7d8599712
Hugo Chinchilla <hugoasecas@gmail.com>
2020-08-21 08:57:23 +0200

    add script to setup xrander in desktop computer

---
+++ i3/.config/i3/scripts/setup-desktop.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sh $HOME/.screenlayout/desktop.sh
---
+++ screenlayout/.screenlayout/desktop.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DVI-D-0 --off --output HDMI-A-1 --mode 2560x1440 --pos 2560x0 --rotate normal --output HDMI-A-0 --off
