5a19b1258d7a8742c8ed1873300e8cc995bd8914
Hugo Chinchilla <hugoasecas@gmail.com>
2021-03-21 23:08:48 +0100

    fix errors in check-dependencies script

--- check-dependencies.sh
+++ check-dependencies.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 PROGRAMS="stow terminator xfce4-screenshooter xbindkeys xautolock scrot convert dbus-send i3lock compton nitrogen zsh amixer"
 
@@ -8,9 +8,9 @@ SUCCESS="✔"
 . ./colors.sh
 
 for program in $PROGRAMS; do
-    if [ $(which $program) ]; then
-        echo "${txtgrn}${SUCCESS}${txtrst} $program"
+    if which $program &> /dev/null; then
+        echo -e "${txtgrn}${SUCCESS}${txtrst} $program"
     else
-        echo "${txtred}${ERROR}${txtrst} $program"
+        echo -e "${txtred}${ERROR}${txtrst} $program"
     fi
 done;
