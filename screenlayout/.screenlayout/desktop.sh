5fdc85ba8cae17233d368ba3d8110b6a7bf8883b
Hugo Chinchilla <hugoasecas@gmail.com>
2023-01-09 16:22:27 +0100

    adjust monitor position

--- screenlayout/.screenlayout/desktop.sh
+++ screenlayout/.screenlayout/desktop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output HDMI-A-0 --off --output HDMI-A-1 --off --output DVI-D-0 --off --output DisplayPort-2 --primary --mode 2560x1440 --pos 0x560 --rotate normal --output DisplayPort-3 --off --output DisplayPort-4 --mode 2560x1440 --pos 2560x0 --rotate left --output DisplayPort-5 --off
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output HDMI-A-0 --off --output HDMI-A-1 --off --output DVI-D-0 --off --output DisplayPort-2 --primary --mode 2560x1440 --pos 0x747 --rotate normal --output DisplayPort-3 --off --output DisplayPort-4 --mode 2560x1440 --pos 2560x0 --rotate left --output DisplayPort-5 --off
